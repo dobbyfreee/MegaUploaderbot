d8a600bcf0675c59707be044b153ec38eb52421e
dobbyfreee <95238089+dobbyfreee@users.noreply.github.com>
2021-12-01 18:20:05 +0530

    Update botMSG.py

--- botModule/botMSG.py
+++ botModule/botMSG.py
@@ -6,15 +6,15 @@ class BotMessage(object):
 
     '''Some permanent Variables defined'''
 
-    common_text = "\n\n<b><u>If you are facing any problem😫, so report📝 at @AJPyroVerseGroup</u></b>"
+    common_text = "\n\n<b><u>If you are facing any problem🤤, so report📝 nje pm cheytho </u></b>"
 
-    to_login = "<b>If you are not logged in then, send login detail in this format email,password.</b>\n"
+    to_login = "<b>🤤If you are not logged in then, send login detail in this format email,password.</b>\n"
 
-    start_msg = f"<b>Hi, I am MegaUploaderBot🤖 Created by @AJPyroVerse and My Developer🧑‍💻 is @AJTimePyro.</b>\n\nAnd I support:-\n1. <u>Direct Downloading Link</u>\n2.<u>Telegram File</u>\n3. <u>Youtube URL</u>\n\n\n{to_login}\n😊We will store your login detail on our database.{common_text}"
+    start_msg = f"<b>Hi, bro ith MegaUploaderBot Anu😌🤖 Created by @Rihu_mone .</b>\n\nAnd I support:-\n1. <u>Direct Downloading Link</u>\n2.<u>Telegram File</u>\n3. <u>Youtube URL</u>\n\n\n{to_login}\n😊We will store your login detail on our database.{common_text}"
 
     help_msg = f"{to_login}\n<b>After login😊 send Direct Downloading Link, Youtube URL or any Telegram File.\n\nTo remove your account from Database use /revoke.</b>{common_text}"
 
-    userBanned = f"<b>You are Banned🚫 from AJPyroVerse Community.</b>"
+    userBanned = f"<b>🤤You are Banned🚫  Community.</b>"
 
     trying_to_login = "<code>I am trying to login your account.\nSo Please Wait...</code>"
 
